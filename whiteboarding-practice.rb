7122a11b197f98a90df4d3b6d8a8fc37227d68ac
ryan <ball.ryan.rb@gmail.com>
2021-08-09 14:43:57 -0600

    adds double algorithm

--- whiteboarding-practice.rb
+++ whiteboarding-practice.rb
@@ -53,3 +53,15 @@ def reduce(numbers)
 end
 
 p reduce([5, 20, 88, 10, 4, 3, 3])
+
+# Map: Double
+# Given an array of numbers, write a function that returns a new array whose values are the original array’s value doubled.
+
+# Input: [4, 2, 5, 99, -4]
+# Output: [8, 4, 10, 198, -8]
+
+def double(numbers)
+  numbers.map { |number| number * 2 }
+end
+
+p double([4, 2, 5, 99, -4])
