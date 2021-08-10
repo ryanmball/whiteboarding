a8110fc6837ce361b93fd7cdefedcac2e543aee1
ryan <ball.ryan.rb@gmail.com>
2021-08-10 15:10:24 -0600

    adds Reduce: Product algorithm

--- whiteboarding-practice.rb
+++ whiteboarding-practice.rb
@@ -135,3 +135,20 @@ end
 p max([5, 17, -4, 20, 12])
 
 
+# Reduce: Product
+# Write a function that accepts an array of numbers and returns the product of all the numbers.
+
+# Input: [1, 2, 3, 4]
+# Output: 24
+
+# Explanation: (1 x 2 x 3 x 4) = 24
+
+def product(numbers)
+  product = 1
+  numbers.each { |number| product *= number }
+  return product
+end
+
+p product([1, 2, 3, 4])
+
+p [1, 2, 3, 4].reduce(:*)
