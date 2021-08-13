b7fc8bc99330826189118bb3b0075291fcc51cab
ryan <ball.ryan.rb@gmail.com>
2021-08-13 17:59:58 -0600

    adds RNA Transcription algorithm

--- whiteboarding-practice.rb
+++ whiteboarding-practice.rb
@@ -173,4 +173,24 @@ p [1, 2, 3, 4].reduce(:*)
 # SOLUTION HERE
 
 
-# 
+# RNA Transcription
+
+# Given a DNA strand, return its RNA complement (per RNA transcription).
+
+# Both DNA and RNA strands are a sequence of nucleotides. Here we're representing the sequences with single-letter characters (e.g. a sample strand may look like: "AGCA".)
+
+# Given a string representing a DNA strand, provide its transcribed RNA strand, according to the following pattern:
+
+# G becomes C
+# C becomes G
+# T becomes A
+# A becomes U
+
+# So based on all this, here's a sample input/output:
+
+# Input: 'ACGTGGTCTTAA'
+# Output: 'UGCACCAGAAUU'
+
+# SOLUTION HERE
+
+
