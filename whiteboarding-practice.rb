ab1c2ab0f6334ab96102ea42b987b71ef34e594f
ryan <ball.ryan.rb@gmail.com>
2021-08-25 09:38:18 -0600

    adds solution to Complete the Data I algorithm

--- whiteboarding-practice.rb
+++ whiteboarding-practice.rb
@@ -182,6 +182,7 @@ end
 
 p popular(posts)
 
+
 # RNA Transcription
 
 # Given a DNA strand, return its RNA complement (per RNA transcription).
@@ -219,22 +220,23 @@ end
 
 p transcription("ACGTGGTCTTAA")
 
+
 # Complete the Data I
 
 # Given an array of social media posts and a hash of users, return a list of posts (as an array of hashes) that replaces the submitted_by id number as the person's actual name.
 
 # For example, given this array of posts (note that the submitted_by is an id number):
 
-# [
-# {title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
-# {title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
-# {title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
-# {title: 'Mondays are the worst', submitted_by: 403, likes: 644}
-# ]
+posts_arr = [
+{title: 'Me Eating Pizza', submitted_by: 231, likes: 1549},
+{title: 'i never knew how cool i was until now', submitted_by: 989, likes: 3},
+{title: 'best selfie evar!!!', submitted_by: 111, likes: 1092},
+{title: 'Mondays are the worst', submitted_by: 403, likes: 644}
+]
 
 # And this hash of users (the key is the id number and the value is the user's real name):
 
-# users = {403 => "Aunty Em", 231 => "Joelle P.", 989 => "Lyndon Johnson", 111 => "Patti Q."}
+users_hash = {403 => "Aunty Em", 231 => "Joelle P.", 989 => "Lyndon Johnson", 111 => "Patti Q."}
 
 # Return the array of posts as follows:
 
@@ -245,4 +247,11 @@ p transcription("ACGTGGTCTTAA")
 # {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
 # ]
 
-# SOLUTION HERE
+def add_user_name(posts, users)
+  posts.each do |post|
+    post[:submitted_by] = users[post[:submitted_by]]
+  end
+  posts
+end
+
+p add_user_name(posts_arr, users_hash)
